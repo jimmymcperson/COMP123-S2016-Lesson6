21deee362ef7c4fdb303abcf6e359bfddbf9c122
jimmymcperson <undeadwalrus@hotmail.com>
2016-06-17 20:27:05 -0400

    Created game structure

---
+++ PlayerProject/ShootingEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlayerProject
+{
+    public class ShootingEnemy : Enemy, ITouchable, IShootable
+    {
+        public ShootingEnemy(string name)
+            : base(name)
+        {
+            
+        }
+
+        public void Shoot()
+        {
+            Console.WriteLine("{0} is shooting", this.Name);
+        }
+
+        public void Fight()
+        {
+            Console.WriteLine("{0} is fighting", this.Name);
+        }
+    }
+}
